afd2cc09414410f63bfc7f85abac9bdd6e41d548
Tomas N Ridilenir <tomridilenir@Mac.fritz.box>
2025-06-12 17:08:27 +0200

    SSR Version with config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,16 @@ import node from "@astrojs/node";
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+      server: {
+        port: process.env.PORT || 3000, // Default to 3000 if PORT is not set
+        host: true // Bind to 0.0.0.0
+    }
   },
   
   adapter: node({
-    mode: 'standalone', 
+    mode: 'standalone',
+    hostname: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
   }),
 
   integrations: [react()],
